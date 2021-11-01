a32c6464e908e3b63ccdf5e5e6940295ff685355
chenshuisenlin <csslneuq@163.com>
2021-11-01 20:47:47 +0800

    leetcode学习环境搭建

---
+++ src/test/java/com/leetcode/study/letsgo/LetsgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode.study.letsgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
