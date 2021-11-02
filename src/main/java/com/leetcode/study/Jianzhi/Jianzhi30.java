0be5649f5cd4ede579b9fcf014e306dfb34aeedf
chenshuisenlin <csslneuq@163.com>
2021-11-02 17:39:51 +0800

    剑指30 补充一下大神的代码 我为啥就不会用Stack 长点记性哦 铁汁

--- src/main/java/com/leetcode/study/Jianzhi/Jianzhi30.java
+++ src/main/java/com/leetcode/study/Jianzhi/Jianzhi30.java
@@ -42,4 +42,27 @@ class MinStack {
     public int min() {
         return minnum.getLast();
     }
+    /**
+     * Stack<Integer> A, B;
+     *     public MinStack() {
+     *         A = new Stack<>();
+     *         B = new Stack<>();
+     *     }
+     *     public void push(int x) {
+     *         A.add(x);
+     *         if(B.empty() || B.peek() >= x)
+     *             B.add(x);
+     *     }
+     *     public void pop() {
+     *         if(A.pop().equals(B.peek()))
+     *             B.pop();
+     *     }
+     *     public int top() {
+     *         return A.peek();
+     *     }
+     *     public int min() {
+     *         return B.peek();
+     *     }
+     *
+     */
 }
