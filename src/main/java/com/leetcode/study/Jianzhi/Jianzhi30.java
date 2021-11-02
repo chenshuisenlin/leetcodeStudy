d294cfcc03d332ba209e05355be31062515d5d74
chenshuisenlin <csslneuq@163.com>
2021-11-02 17:35:37 +0800

    剑指30 体感还行 比上一题写得好一些 小细节还需要注意

---
+++ src/main/java/com/leetcode/study/Jianzhi/Jianzhi30.java
@@ -0,0 +1,45 @@
+package com.leetcode.study.Jianzhi;
+
+import java.util.LinkedList;
+
+public class Jianzhi30 {
+}
+
+class MinStack {
+    LinkedList<Integer> A;
+    LinkedList<Integer> minnum;
+    /** initialize your data structure here. */
+    public MinStack() {
+        A=new LinkedList<>();
+        minnum=new LinkedList<>();
+        //minnum=0;
+    }
+
+    public void push(int x) {
+        if(A.isEmpty()){
+            minnum.addLast(x);
+        }else{
+            int last=minnum.getLast();
+            //注意多个相同最小值，防止一次弹出最小值之后最小栈为空
+            if(x<=last){
+                minnum.addLast(x);
+            }
+        }
+        A.addLast(x);
+    }
+
+    public void pop() {
+        int re=A.removeLast();
+        if(minnum.getLast()==re){
+            minnum.removeLast();
+        }
+    }
+
+    public int top() {
+        return A.getLast();
+    }
+
+    public int min() {
+        return minnum.getLast();
+    }
+}
