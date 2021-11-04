e2884a5f673c9f689fa614821e9fd0276ece1968
chenshuisenlin <csslneuq@163.com>
2021-11-04 14:20:44 +0800

    测试不同步代码

---
+++ src/main/java/com/leetcode/study/Jianzhi/Jianzhi35.java
@@ -0,0 +1,15 @@
+package com.leetcode.study.Jianzhi;
+
+public class Jianzhi35 {
+}
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
