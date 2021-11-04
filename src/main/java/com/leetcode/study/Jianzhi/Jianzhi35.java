0638885d4cf4778d22886b209610797dc1d9a7bf
chenshuisenlin <csslneuq@163.com>
2021-11-04 14:50:57 +0800

    测试修改不同步代码

--- src/main/java/com/leetcode/study/Jianzhi/Jianzhi35.java
+++ src/main/java/com/leetcode/study/Jianzhi/Jianzhi35.java
@@ -1,6 +1,31 @@
 package com.leetcode.study.Jianzhi;
 
+import java.util.LinkedList;
+
 public class Jianzhi35 {
+    public Node copyRandomList(Node head) {
+        Node res = null;
+        Node cur = null;
+        Node temp=head;
+        LinkedList<Integer> list=new LinkedList<>();
+        LinkedList<Integer> listR=new LinkedList<>();
+        while(temp!=null){
+            list.addLast(temp.val);
+            listR.addLast(temp.random.val);
+            temp=temp.next;
+
+        }
+        while (!list.isEmpty()){
+            cur.val=list.removeLast();
+            cur.next=res;
+            res=cur;
+        }
+        while (!listR.isEmpty()){
+            int target=listR.getFirst();
+
+        }
+        return res;
+    }
 }
 class Node {
     int val;
