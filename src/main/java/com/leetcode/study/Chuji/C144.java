d3d964945d1bda241b0c087c86fcd1deb2356abf
chenshuisenlin <csslneuq@163.com>
2021-11-08 14:52:23 +0800

    144 经典简单题 二叉树的遍历 递归以及迭代两种方法

---
+++ src/main/java/com/leetcode/study/Chuji/C144.java
@@ -0,0 +1,62 @@
+package com.leetcode.study.Chuji;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class C144 {
+    /**
+     * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+     * 前左右
+     * 迭代
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        Stack<TreeNode> stack=new Stack<>();
+        stack.push(root);
+        while (!stack.empty()){
+            TreeNode node=stack.pop();
+            list.add(node.val);
+            if(node.right!=null){
+                stack.push(node.right);
+            }
+            if(node.left!=null){
+                stack.push(node.left);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 递归
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        preorder(root,list);
+        return list;
+    }
+    public void preorder(TreeNode node,List<Integer> list){
+        if(node==null){
+            return;
+        }
+        list.add(node.val);
+        preorder(node.left,list);
+        preorder(node.right,list);
+    }
+    class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+}
