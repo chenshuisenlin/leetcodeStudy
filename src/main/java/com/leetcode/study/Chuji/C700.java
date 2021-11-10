b7a4c7cc2b7582cb50c8e1f9a79049977ec8eeb9
chenshuisenlin <csslneuq@163.com>
2021-11-10 17:29:53 +0800

    二叉搜索树查找目标子树

---
+++ src/main/java/com/leetcode/study/Chuji/C700.java
@@ -0,0 +1,35 @@
+package com.leetcode.study.Chuji;
+
+public class C700 {
+    /**
+     * 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+     * @param root
+     * @param val
+     * @return
+     */
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null){
+            return null;
+        }
+        if(root.val==val){
+            return root;
+        }else if(root.val>val){
+            return searchBST(root.left,val);
+        }else {
+            return searchBST(root.right,val);
+        }
+
+    }
+    class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+}
