e89ceaefa06661f05b3cab3c9d5a0b214eddaffa
chenshuisenlin <csslneuq@163.com>
2021-11-19 17:09:11 +0800

    5题 中等难度 最长回文子串问题 解法颇多 时间复杂度从O(n^3) 到O(n)

---
+++ src/main/java/com/leetcode/study/Chuji/C5.java
@@ -0,0 +1,215 @@
+package com.leetcode.study.Chuji;
+
+public class C5 {
+    public static void main(String[] args) {
+        C5 c5 = new C5();
+        System.out.println(c5.longestPalindrome("babad"));
+    }
+    public String preProcess(String s) {
+        int n = s.length();
+        if (n == 0) {
+            return "^$";
+        }
+        String ret = "^";
+        for (int i = 0; i < n; i++)
+            ret += "#" + s.charAt(i);
+        ret += "#$";
+        return ret;
+    }
+
+    // 马拉车算法
+    public String longestPalindrome5(String s) {
+        String T = preProcess(s);
+        int n = T.length();
+        int[] P = new int[n];
+        int C = 0, R = 0;
+        for (int i = 1; i < n - 1; i++) {
+            int i_mirror = 2 * C - i;
+            if (R > i) {
+                P[i] = Math.min(R - i, P[i_mirror]);// 防止超出 R
+            } else {
+                P[i] = 0;// 等于 R 的情况
+            }
+
+            // 碰到之前讲的三种情况时候，需要利用中心扩展法
+            while (T.charAt(i + 1 + P[i]) == T.charAt(i - 1 - P[i])) {
+                P[i]++;
+            }
+
+            // 判断是否需要更新 R
+            if (i + P[i] > R) {
+                C = i;
+                R = i + P[i];
+            }
+
+        }
+
+        // 找出 P 的最大值
+        int maxLen = 0;
+        int centerIndex = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (P[i] > maxLen) {
+                maxLen = P[i];
+                centerIndex = i;
+            }
+        }
+        int start = (centerIndex - maxLen) / 2; //最开始讲的求原字符串下标
+        return s.substring(start, start + maxLen);
+    }
+
+    /**
+     * 中心扩散算法
+     * @param s
+     * @return
+     */
+    public String longestPalindrome4(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
+
+    // 暴力解法
+    public String longestPalindrome3(String s) {
+        int n = s.length();
+        String res = "";
+        boolean[] P = new boolean[n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= i; j--) {
+                P[j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || P[j - 1]);
+                if (P[j] && j - i + 1 > res.length()) {
+                    res = s.substring(i, j + 1);
+                }
+            }
+        }
+        return res;
+
+    }
+
+    public String longestPalindrome2(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+        // dp[i][j] 表示 s[i..j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        // 初始化：所有长度为 1 的子串都是回文串
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        char[] charArray = s.toCharArray();
+        // 递推开始
+        // 先枚举子串长度
+        for (int L = 2; L <= len; L++) {
+            // 枚举左边界，左边界的上限设置可以宽松一些
+            for (int i = 0; i < len; i++) {
+                // 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
+                int j = L + i - 1;
+                // 如果右边界越界，就可以退出当前循环
+                if (j >= len) {
+                    break;
+                }
+
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+
+    /**
+     * 给你一个字符串 s，找到 s 中最长的回文子串。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：s = "babad"
+     * 输出："bab"
+     * 解释："aba" 同样是符合题意的答案。
+     * 示例 2：
+     *
+     * 输入：s = "cbbd"
+     * 输出："bb"
+     * 示例 3：
+     *
+     * 输入：s = "a"
+     * 输出："a"
+     * 示例 4：
+     *
+     * 输入：s = "ac"
+     * 输出："a"
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+        char[] arr=s.toCharArray();
+        int left=0;
+        int right=0;
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                if(isHui(arr,i,j)){
+                    System.out.println("发现回文子串"+s.substring(i,j+1));
+                    if(j-i>=right-left){
+                        left=i;
+                        right=j;
+                    }
+                }
+            }
+            if(right-left>s.length()-i){
+                break;
+            }
+        }
+
+        return s.substring(left,right+1);
+    }
+    public boolean isHui(char[] arr,int i,int j){
+        while (i<=j){
+            if(arr[i]!=arr[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
